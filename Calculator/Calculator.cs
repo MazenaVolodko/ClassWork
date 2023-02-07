936fe193dc73f92f0c29c3a9d36ed7f71deef649
Mažena <Maena@LAPTOP-P4NVMIA3>
2023-02-07 20:36:31 +0200

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public static int Sum(int number1, int number2)
+        { 
+            return number1 + number2; 
+        }
+
+        public static int Difference(int number1, int number2)
+        {
+          return number1 - number2;
+        }
+        public static int Product(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public static double Quontient(int number1, int number2)
+        { 
+        return (double)number1 / number2;
+        }
+
+    }
+}
---
+++ CalculatorUnitTests/UnitTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CalculatorUnitTests
+{
+    public class UnitTests
+    {
+        [Test]
+        public void Sum1And1()
+        {
+            int expectedResult = 2;
+
+            int actualResult = Calculator.Calculator.Sum(1, 1);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
